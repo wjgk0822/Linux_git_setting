3a525257411ddac0747dc06d24d948bbe9e30886
wjgk0822 <35884296+wjgk0822@users.noreply.github.com>
2024-02-09 00:18:40 +0900

    Coding test
    Add two value with array

---
+++ coding test_prac/7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int length, input;
+
+	int start, end;
+
+	int sum = 0;
+
+	int arr[10] = {0};
+	
+	cin >> length>>input;
+
+
+	for (int i = 0; i < length; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < input; i++)
+	{
+		cin >> start >> end;
+		for (int j = start; j <= end; j++)
+		{
+			sum += arr[j-1];
+		}
+		printf("%d\n", sum);
+		sum = 0;
+
+		//1 3->5+4+3    2 4->4+3+2   5 5->1
+		//
+	}
+}
